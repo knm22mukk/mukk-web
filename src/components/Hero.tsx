61b6d08fb9866c9eb5ae862f1baab4601ce79fe9
knm22mukk <knm22mukk@gmail.com>
2022-04-19 22:50:22 +0900

    HEROの表示の変更

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,41 +4,32 @@ import { VFC } from 'react';
 
 const Hero: VFC = () => {
   return (
-    <div className='container'>
-      <div className='flex flex-col py-20 mx-auto max-w-screen-xl md:py-24 lg:flex-row lg:items-center'>
-        <div className=' mx-auto max-w-lg text-center lg:w-5/12 lg:max-w-none lg:text-left'>
-          <h1 className='text-3xl font-bold leading-tight md:text-3xl lg:text-4xl xl:text-5xl'>
-            Information on
-            <span className='text-indigo-500'> packaging materials</span>
+    <div className='container relative'>
+      <div className='flex flex-col py-20 mx-auto max-w-screen-xl md:items-center md:py-24 lg:flex-row'>
+        <div className='relative shrink-0 text-center lg:pr-12 lg:w-6/12 lg:text-left'>
+          <h1 className='max-w-3xl text-3xl font-black leading-snug md:text-5xl'>
+            Modern React Templates, Just For You
           </h1>
-          <p className='my-5 text-base lg:my-8 xl:text-lg'>
-            包装資材営業マンのむっくが新商品や市場動向などを紹介する個人ブログサイトです。
+          <p className='my-5 mx-auto max-w-lg text-sm font-medium text-gray-600 lg:my-8 lg:mx-0 lg:text-base'>
+            Our templates are easy to setup, understand and customize. Fully modular components with
+            a variety of pages and components.
           </p>
-          <div className='flex justify-center lg:justify-start'>
-            <div className='px-2'>
-              <Link href='/blog'>
-                <a>
-                  <button className='btn'>Blog</button>
-                </a>
-              </Link>
-            </div>
-            <div className='px-2'>
-              <Link href='/contact'>
-                <a>
-                  <button className='btn'>Contact</button>
-                </a>
-              </Link>
-            </div>
+          <div className='flex flex-col justify-center items-center mt-8 sm:flex-row lg:justify-start'>
+            <Link href='/blog'>
+              <a>
+                <button className='btn'>ブログ一覧へ</button>
+              </a>
+            </Link>
           </div>
         </div>
-        <div className='flex flex-col flex-1 justify-center mt-12 lg:self-end lg:mt-0'>
-          <div className='flex justify-center items-center lg:justify-end'>
+        <div className='flex relative flex-col justify-center mt-12 lg:mt-0'>
+          <div className='flex relative justify-center items-center max-w-3xl md:justify-end lg:max-w-none'>
             <Image
               src='/images/mukkblog_topimage_640x350.webp'
               alt='HeroImage'
-              width={650}
-              height={450}
-              className=''
+              width={640}
+              height={350}
+              objectFit='cover'
             />
           </div>
         </div>
