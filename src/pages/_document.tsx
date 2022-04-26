7b4e85d505d8281f50cd97e6c845b5eb3624e62b
knm22mukk <knm22mukk@gmail.com>
2022-04-26 23:05:21 +0900

    favicon, apple-touch-iconの設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,17 @@ class MyDocument extends Document {
           <meta name='apple-mobile-web-app-title' content='' />
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='theme-color' content='' />
+          <meta name='theme-color' content='#3b82f6' />
+
+          <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+          <link rel='manifest' href='/favicons/site.webmanifest' crossOrigin='use-credentials' />
+          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#000000' />
+          <link rel='shortcut icon' href='/favicons/favicon.ico' />
+          <meta name='msapplication-TileColor' content='#ffffff' />
+          <meta name='msapplication-config' content='/favicons/browserconfig.xml' />
+          <meta name='theme-color' content='#ffffff' />
         </Head>
         <body className='text-gray-800 dark:text-white bg-gray-50 dark:bg-gray-800'>
           <Main />
