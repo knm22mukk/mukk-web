b8db07a40dfcfc228bb973518328f81e8fe276e1
knm22mukk <knm22mukk@gmail.com>
2022-05-11 23:14:31 +0900

    ブログカードのバナー画像を変更

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -7,20 +7,25 @@ const BlogCard = () => {
       <a>
         <div className='mx-auto max-w-sm'>
           <div className='hover:text-indigo-600 rounded-lg border border-gray-200 shadow-md transition duration-500 hover:scale-105'>
-            <Image
-              src='/images/blog_banner384x216.webp'
-              width={384}
-              height={216}
-              alt='BlogImage'
-              className='rounded-t-lg'
-            />
+            <div className='relative'>
+              <Image
+                src='/images/blog_banner384x216.webp'
+                width={384}
+                height={216}
+                alt='BlogImage'
+                className='rounded-t-lg hover:opacity-80'
+              />
+              <h5 className='absolute top-1/3 px-6 text-2xl font-bold text-white'>
+                JAMスタックな包装資材ブログをつくってみた
+              </h5>
+            </div>
             <div className='p-5'>
               <div className='mt-4 text-xs italic font-semibold text-gray-600 dark:text-white uppercase'>
                 april 4, 2022
               </div>
-              <h5 className='mt-5 text-2xl font-black hover:text-indigo-600 transition duration-300'>
+              <p className='mt-5 text-xl font-black hover:text-indigo-600'>
                 〇〇から発売された新商品をご紹介します
-              </h5>
+              </p>
             </div>
           </div>
         </div>
