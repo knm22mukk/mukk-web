f30c68527ad11074b13e57228a9c9cc1b72c3ae4
knm22mukk <knm22mukk@gmail.com>
2022-05-30 22:52:42 +0900

    ドメインの設定、GAの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import '@styles/globals.css';
+import { GoogleAnalytics, usePageView } from '@libs/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
   return (
-    <ThemeProvider attribute='class' defaultTheme='light'>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute='class' defaultTheme='light'>
+        <GoogleAnalytics />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
