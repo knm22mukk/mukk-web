d25bc1e5e3e18c45ecb93918edca182472b02520
knm22mukk <knm22mukk@gmail.com>
2022-05-31 23:11:28 +0900

    next-sitemapの導入

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://mukkpack.com',
+  changefreq: 'weekly',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './out',
+};
