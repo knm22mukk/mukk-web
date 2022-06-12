d24385d5720c8373615a2e64dea564ab7468a55d
knm22mukk <knm22mukk@gmail.com>
2022-06-12 23:09:03 +0900

    objectFit=coverを追加しました

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,13 @@ const Hero: VFC = () => {
         </div>
         <div className='flex flex-col justify-center mt-12 lg:mt-0'>
           <div className='flex justify-center items-center max-w-3xl md:justify-end lg:max-w-none'>
-            <Image src='/images/heroImage.webp' alt='HeroImage' width={500} height={430} />
+            <Image
+              src='/images/heroImage.webp'
+              alt='HeroImage'
+              width={500}
+              height={430}
+              objectFit='cover'
+            />
           </div>
         </div>
       </div>
--- src/components/ToContact.tsx
+++ src/components/ToContact.tsx
@@ -14,6 +14,7 @@ const ToContact: VFC = () => {
               width={500}
               height={500}
               alt='ContactUs'
+              objectFit='cover'
               className='rounded-lg'
             />
           </div>
