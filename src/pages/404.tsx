e539b2294c23c72c5de4623c4dc1f8a78fc9843f
knm22mukk <knm22mukk@gmail.com>
2022-06-13 22:11:56 +0900

    404ページを修正しました

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,22 +9,18 @@ const Custom404: NextPage = () => {
       <Head>
         <title>MUKK PACK || ページが見つかりませんでした</title>
       </Head>
-      <div className='flex justify-center items-center w-screen h-screen bg-gradient-to-r from-indigo-600 to-pink-400'>
-        <div className='container'>
-          <div className='py-20 bg-white rounded-md shadow-xl'>
-            <div className='flex flex-col items-center'>
-              <Image src='/images/404page.webp' alt='404' width={300} height={300} />
-              <h3 className='mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl'>
-                おっと! <span className='text-red-500'>ページが見つかりませんでした！</span>
-              </h3>
-              <p className='mb-8 text-center text-gray-500 md:text-lg'>
-                The page youre looking for doesnt exist.
-              </p>
-              <Link href='/'>
-                <a className='p-2 btn'>ホームに戻る</a>
-              </Link>
-            </div>
-          </div>
+      <div className='container flex justify-center items-center w-screen h-screen'>
+        <div className='flex flex-col items-center'>
+          <Image src='/images/404image.png' alt='404' width={600} height={400} />
+          <h3 className='my-2 text-2xl font-bold text-center text-gray-800 md:text-3xl'>
+            おっと! <span className='text-red-500'>ページが見つかりませんでした！</span>
+          </h3>
+          <p className='mb-8 text-center text-gray-500 md:text-lg'>
+            The page youre looking for doesnt exist.
+          </p>
+          <Link href='/'>
+            <a className='p-2 btn'>ホームに戻る</a>
+          </Link>
         </div>
       </div>
     </>
